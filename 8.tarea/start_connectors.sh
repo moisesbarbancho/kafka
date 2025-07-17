55efe03eca775e1fc0d572951058f3ca1e2245dc
Barbancho, Moisés <moisesbarbancho@gmail.com>
2025-07-17 05:23:40 -0400

    datagen sensor-telemetry done

--- 8.tarea/start_connectors.sh
+++ 8.tarea/start_connectors.sh
@@ -6,7 +6,7 @@ curl -d @"./connectors/source-datagen-_transactions.json" -H "Content-Type: appl
 
 curl -d @"./connectors/sink-mysql-_transactions.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
 
-#curl -d @"./connectors/source-datagen-sensor-telemetry.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
+curl -d @"./connectors/source-datagen-sensor-telemetry.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
 
 #curl -d @"./connectors/source-mysql-transactions.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
 
