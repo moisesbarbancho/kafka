c55b5f3c295e9b6e0c7a2f772932a7c9830c21cc
Barbancho, Moisés <moisesbarbancho@gmail.com>
2025-07-18 02:12:09 -0400

    source mysql start

--- 8.tarea/start_connectors.sh
+++ 8.tarea/start_connectors.sh
@@ -8,7 +8,7 @@ curl -d @"./connectors/sink-mysql-_transactions.json" -H "Content-Type: applicat
 
 curl -d @"./connectors/source-datagen-sensor-telemetry.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
 
-#curl -d @"./connectors/source-mysql-transactions.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
+curl -d @"./connectors/source-mysql-transactions.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors | jq
 
 echo "OK"
 
